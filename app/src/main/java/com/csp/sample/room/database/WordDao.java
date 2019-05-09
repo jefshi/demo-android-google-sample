2fd8db583d15a98b836e23cf5fd32e49942ced90
jefshi <jefshi@yeah.net>
2019-05-09 16:28:17 +0800

    [room-with-a-view-java]: 追加 ViewModel

--- app/src/main/java/com/csp/sample/room/database/WordDao.java
+++ app/src/main/java/com/csp/sample/room/database/WordDao.java
@@ -1,10 +1,8 @@
 package com.csp.sample.room.database;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
