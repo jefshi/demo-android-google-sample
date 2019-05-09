0d98a1c0bbdcce5e6daf2fa6e82d325cbd029c33
jefshi <jefshi@yeah.net>
2019-05-09 18:06:45 +0800

    [room-with-a-view-java]: 追加 XML 和 RecyclerView

--- app/src/main/java/com/csp/sample/room/MainActivity.java
+++ app/src/main/java/com/csp/sample/room/MainActivity.java
@@ -2,12 +2,19 @@ package com.csp.sample.room;
 
 import android.app.Activity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
