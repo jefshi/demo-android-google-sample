db544d73088193877cc692e6419b55acf9c15e56
jefshi <jefshi@yeah.net>
2019-05-13 17:18:30 +0800

    [room-with-a-view]: 补充

--- app/src/main/java/com/csp/sample/room/database/WordDao.java
+++ app/src/main/java/com/csp/sample/room/database/WordDao.java
@@ -2,6 +2,7 @@ package com.csp.sample.room.database;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
@@ -16,12 +17,21 @@ public interface WordDao {
     @Insert
     void insert(Word word);
 
+    @Delete()
+    void delete(Word word);
+
     @Query("DELETE FROM word_table")
     void deleteAll();
 
     // the ViewModel only exposes immutable LiveData objects to the observers
     @Query("SELECT * from word_table ORDER BY word ASC")
     LiveData<List<Word>> getAllWords();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    List<Word> getAll();
+
+    @Query("SELECT * from word_table WHERE word like :contain")
+    List<Word> getWords(String contain);
 
 //    // the developer want to update the stored data, you must use MutableLiveData instead of LiveData
 //    @Query("SELECT * from word_table ORDER BY word ASC")
